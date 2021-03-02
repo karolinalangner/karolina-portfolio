d0dfd3679701159eeb8fb3eb566ddc522ac74b63
karolina langner <karolinalangner1@gmail.com>
2021-03-02 16:20:08 +0000

    views ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def bio
+  end
+
+  def contact
+  end
+
+  def skills
+  end
+
+  def projects
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'bio', to: 'pages#bio'
+  get 'skills', to: 'pages#skills'
+  get 'projects', to: 'pages#projects'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
