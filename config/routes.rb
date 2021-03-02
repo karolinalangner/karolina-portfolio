47ae70b2caf8b8aa240da7b18cef3940d1e242bd
karolina langner <karolinalangner1@gmail.com>
2021-03-02 16:34:38 +0000

    contact form added, not configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   root to: 'pages#home'
+  resources :contacts, only: [:new, :create]
+  
   get 'bio', to: 'pages#bio'
   get 'skills', to: 'pages#skills'
   get 'projects', to: 'pages#projects'
